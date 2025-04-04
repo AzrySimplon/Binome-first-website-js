9e6535cae734f00b1ff054d80e5275371a767b59
AzrySimplon <azriel.simplon@gmail.com>
2025-04-04 13:27:41 +0200

    filterArray, changeColorMode, activeInactive

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const colorsArray = {
+    textTitle: ["hsl(227, 75%, 14%)", "hsl(200, 60%, 99%)"],
+    bgGradient: ["linear-gradient(180deg, #EBF2FC 0%, #EEF8F9 100%)", "linear-gradient(180deg, #040918 0%, #091540 100%)"],
+}
+
+
+
+
+
 async function fetchData() {
     const response = await fetch("data.json");
     const data = await response.json();
@@ -24,7 +33,42 @@ function populateSection(array, section) {
     });
 }
 
+function filterArray(state) {
+    let returnArray;
+    if (state === true || state === false) {
+        returnArray = dataJSON.filter((obj) => obj.isActive === state);
+    }
+    else {
+        returnArray = dataJSON;
+    }
+    populateSection(returnArray, extensionsSection);
+}
+
+function changeColorMode() {
+    let index;
+    if (activeMode === "light") {
+        index = 0;
+    }
+    else {
+        index = 1;
+    }
+
+    /*document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');*/
+
+    //edit button image
+
+}
+
+function activeInactive(id) {
+    console.log(id);
+    const listIndex = dataJSON.findIndex((obj) => obj.name === id);
+    dataJSON[listIndex].isActive = !dataJSON[listIndex].isActive;
+}
 
 const extensionsSection = document.getElementById("extensions-section");
+let dataJSON;
+let activeMode = "light";
 
-fetchData().then(data => populateSection(data, extensionsSection));
+fetchData().then(data => {
+    populateSection(data, extensionsSection); dataJSON = data
+});
